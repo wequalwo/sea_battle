9e941edd01096869c4693aa69dce86aa55dc14d6
Seraf Ivanov <s.k.ivanov314@gmail.com>
2021-12-11 22:44:26 +0300

    v6.6

---
+++ src/com/company/Op_observer.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Op_observer extends Observer
+{
+	Op_observer(boolean whose)
+	{
+		super(whose);
+	}
+	public ArrayList<int[]> shot(int i, int j) throws Exception
+	{
+		return set.shot(i, j);
+	}
+}
