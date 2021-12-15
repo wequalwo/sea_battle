710a14fddb407e9d6e442a69ae4a6538f6069f03
Seraf Ivanov <s.k.ivanov314@gmail.com>
2021-12-15 22:11:46 +0300

    v16.3 eng commented and some little fixes

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 /**
- * Class Board for game op-and player's boards.
+ * Class Board for game. It will be extended by Opponents_board and My_board
  */
 abstract public class Board extends JPanel implements ActionListener, def
 {
